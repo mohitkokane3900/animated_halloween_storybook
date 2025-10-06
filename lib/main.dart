25ab7708830b95bf963bfb9993eca58fd89f2f3b
SophieNguyen113 <sophiengocnguyen113@gmail.com>
2025-10-06 18:09:51 -0400

    App base and spookyboard with message

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,90 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const HalloweenApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HalloweenApp extends StatelessWidget {
+  const HalloweenApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Spooky Hunt',
+      theme: ThemeData.dark(useMaterial3: true),
+      home: const SpookyHome(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class SpookyHome extends StatefulWidget {
+  const SpookyHome({super.key});
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<SpookyHome> createState() => _SpookyHomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _SpookyHomeState extends State<SpookyHome> {
+  String message = 'Find the magic pumpkin';
+  bool won = false;
 
-  void _incrementCounter() {
+  void onTrap() {
+    if (won) return;
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      message = 'Boo! Wrong item';
+    });
+  }
+
+  void onTarget() {
+    if (won) return;
+    setState(() {
+      won = true;
+      message = 'You found it!';
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final size = MediaQuery.sizeOf(context);
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      appBar: AppBar(title: const Text('Halloween Hunt')),
+      body: Stack(
+        children: [
+          Container(decoration: const BoxDecoration(gradient: LinearGradient(colors: [Colors.black, Color(0xFF0B1020)], begin: Alignment.topCenter, end: Alignment.bottomCenter))),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Text(message, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w600)),
             ),
-          ],
-        ),
+          ),
+          Align(
+            alignment: const Alignment(-0.7, -0.2),
+            child: GestureDetector(
+              onTap: onTrap,
+              child: const Icon(Icons.emoji_emotions, size: 72, color: Colors.white),
+            ),
+          ),
+          Align(
+            alignment: const Alignment(0.8, -0.1),
+            child: GestureDetector(
+              onTap: onTrap,
+              child: const Icon(Icons.bedtime, size: 68, color: Colors.blueGrey),
+            ),
+          ),
+          Align(
+            alignment: const Alignment(0.1, 0.2),
+            child: GestureDetector(
+              onTap: onTarget,
+              child: const Icon(Icons.emoji_nature, size: 76, color: Colors.orange),
+            ),
+          ),
+          if (won)
+            Container(
+              width: size.width,
+              height: size.height,
+              color: Colors.black.withOpacity(0.3),
+            ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
